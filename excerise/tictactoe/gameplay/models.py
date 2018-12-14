9d0710c210b46b5b154952568e90bfeaf844de9b
Hoa Vo <whoait@gmail.com>
2018-12-14 13:41:53 +0700

    Custom Model Manager

--- excerise/tictactoe/gameplay/models.py
+++ excerise/tictactoe/gameplay/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
+from django.db.models import Q
 from django.contrib.auth.models import User
 
 
@@ -14,6 +15,18 @@ GAME_STATUS_CHOICES = (
     ('D', 'Draw')
 )
 
+
+class GamesQuerySet(models.QuerySet):
+  def game_for_user(self, user):
+    return self.filter(
+      Q(first_player = user) | Q(second_player = user)
+    )
+  
+  def active(self):
+    return self.filter(
+      Q(status= 'F') | Q(status= 'S')
+    )
+    
 @python_2_unicode_compatible
 class Game(models.Model):
   first_player = models.ForeignKey(User, related_name= "games_first_player", on_delete = models.CASCADE)
@@ -21,6 +34,8 @@ class Game(models.Model):
   start_time = models.DateTimeField(auto_now_add=True)
   last_active = models.DateTimeField(auto_now=True)
   status = models.CharField(max_length=1, default = 'F', choices = GAME_STATUS_CHOICES)
+
+  objects = GamesQuerySet.as_manager()
 
   def __str__(self):
     return "{0} vs {1}".format(
--- excerise/tictactoe/player/views.py
+++ excerise/tictactoe/player/views.py
@@ -3,17 +3,9 @@ from django.shortcuts import render
 from gameplay.models import Game
 # Create your views here.
 def home(request):
-  game_first_player = Game.objects.filter(
-    first_player = request.user,
-    status = 'F'
-  )
-  game_second_player = Game.objects.filter(
-    second_player = request.user,
-    status = 'S'
-  )
-  all_my_games = list(game_first_player) + \
-                 list(game_second_player)
+  my_games = Game.objects.game_for_user(request.user)
+  active_games = my_games.active()
 
 
   return render(request, 'player/home.html',
-                        {'games': all_my_games})
+                        {'games': active_games})
